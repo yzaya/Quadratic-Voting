7e7ecc714b5ba45dd114855dd05400d13bd49705
yzaya <ikuhle@yahoo.com>
2023-05-01 15:05:31 -0400

    adding from local

---
+++ form.js
@@ -0,0 +1,12 @@
+var email = ""
+var ballot_name = ""
+var voteNo = ""
+var ID = ""
+
+function formIn(){
+    email = document.getElementById("emailIn");
+    ballot_name = document.getElementById("ballotIn");
+    voteNo = document.getElementById("voteNoIn");
+};
+
+console.log(email)
